5421b710ac135935f022ac8eb98017b687f4f9df
Santiago Soto <140900819+santi3223@users.noreply.github.com>
2026-04-22 11:22:24 -0400

    USACO GUIDE

---
+++ USACO GUIDE/3 Gold/Knapsack DP/Coin Combinations I.cpp
@@ -0,0 +1,39 @@
+//22 de Abril 2026 10:42
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define ff(aa, bb, cc) for(ll aa = bb; aa < cc; aa++)
+#define ed "\n"
+#define all(aaa) aaa.begin(), aaa.end()
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define vb vector<bool>
+#define pb push_back
+#define vs vector<string>
+constexpr ll MOD = 1e9+7;
+ 
+int main(){
+	ll n, x;
+	cin >> n >> x;
+	vl arr(n);
+	ff(i, 0, n){
+		cin >> arr[i];
+	}
+	vl dp(x+5, 0);
+	dp[0] = 1;
+	ff(i, 1, x+1){
+		ff(j, 0, n){
+			if(i-arr[j] < 0) continue;
+			dp[i] += dp[i-arr[j]];
+			if(dp[i] >= MOD) dp[i] %= MOD;
+		}
+	}
+	/*ff(i, 0, dp.size()){
+		cout << dp[i] << ' ';
+	}
+	cout << ed;*/
+	cout << dp[x];
+	
+}
